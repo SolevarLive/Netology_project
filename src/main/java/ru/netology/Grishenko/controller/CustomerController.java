bbb3cd68e9a7cf839d48a933ba762e6fc82e50d5
Davyd <129197705+SolevarLive@users.noreply.github.com>
2023-11-29 16:06:58 +0500

    CДЕЛАЛ ПОЛНОСТЬЮ CustomerController

--- src/main/java/ru/netology/Grishenko/controller/CustomerController.java
+++ src/main/java/ru/netology/Grishenko/controller/CustomerController.java
@@ -1,9 +1,7 @@
 package ru.netology.Grishenko.controller;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.Grishenko.Domain.Customer;
 import ru.netology.Grishenko.Service.CustomerService;
 import ru.netology.Grishenko.controller.dto.CustomerDTO;
@@ -19,27 +17,41 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @GetMapping
-    public GetClientsResponse getClients(){
-         List<Customer> customers = customerService.getCustomers();
+    public GetClientsResponse getClients() {
+        List<Customer> customers = customerService.getCustomers();
 
 
-
-         List<CustomerDTO> customerDTOS = new ArrayList<>();
-         for(Customer customer: customers){
-            CustomerDTO customerDTO  = new CustomerDTO(customer.getId(), customer.getName());
+        List<CustomerDTO> customerDTOS = new ArrayList<>();
+        for (Customer customer : customers) {
+            CustomerDTO customerDTO = new CustomerDTO(customer.getId(), customer.getName());
             customerDTOS.add(customerDTO);
-         }
+        }
         return new GetClientsResponse(customerDTOS);
     }
 
     @GetMapping("/{id}")
-    public CustomerDTO getCustomer(@PathVariable(name= "id") int customerId){
-        for(Customer customer : customerService.getCustomers()){
-            if (customer.getId() == customerId){
-                CustomerDTO customerDTO =  new CustomerDTO(customer.getId(), customer.getName());
+    public CustomerDTO getCustomer(@PathVariable(name = "id") int customerId) {
+        for (Customer customer : customerService.getCustomers()) {
+            if (customer.getId() == customerId) {
+                CustomerDTO customerDTO = new CustomerDTO(customer.getId(), customer.getName());
                 return customerDTO;
             }
         }
         return null;
     }
+
+    @PostMapping
+    public void addCustomer(@RequestBody CustomerDTO newCustomer) {
+        customerService.addCustomer(newCustomer.getId(), newCustomer.getName());
+        System.out.println("Customer added successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable(name = "id") int customerId) {
+        List<Customer> customers = customerService.getCustomers();
+        customers.removeIf(customer -> customer.getId() == customerId);
+        return ResponseEntity.ok("Customer with ID " + customerId + " has been deleted");
+    }
+
+
 }
